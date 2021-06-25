1a564bd673a34c6691ec0d54cd079e61c46880d1
EricMalpass <ericmalpass@gmail.com>
2021-06-25 16:36:59 -0700

    react added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About";
+import Navbar from "./componets/Navbar";
+import Footer from "./componets/Footer";
+import Wrapper from "./componets/Wrapper";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
