a6dff625c1eb0823fe8aa728499107769c58a786
EricMalpass <ericmalpass@gmail.com>
2021-07-10 11:51:16 -0700

    update

--- src/components/Biography/index.js
+++ src/components/Biography/index.js
@@ -12,9 +12,13 @@ function Biography() {
               <p>
               I was born in Seattle and raised in the greater Seattle area. 
               </p>
-              <p>I attended Western Washington University and Vrije Universiteit Brussel. I have worked in many customer centric business roles and as teacher.
+              <p>I attended Western Washington University and Vrije Universiteit Brussel, graduating with a BA in International Business Management. 
+               <p> I have worked in many customer centric business roles and as teacher.</p>
               </p>
               <p>
+                With Covid I decided to enter the University of Washington to earn a certificate in full stack web development.
+                </p>
+                <p>
               I am looking to transition to a new role using my skills as a full stack developer such as JavaScript, Express, Node, and React.
               </p>
             </article>
